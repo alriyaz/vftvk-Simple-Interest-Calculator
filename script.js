0b25fe7a7bf17bca58549bf99bffbfbe6497bd4e
Riyaz <80829121+alriyaz@users.noreply.github.com>
2021-06-08 16:58:11 +0530

    changes done from local pc

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-function compute()
-{   
+
+function calculate(){   
     var principal=document.getElementById("principal");
     var p = document.getElementById("principal").value;
     var n = document.getElementById("years").value;
@@ -9,14 +9,29 @@ function compute()
 
     var year = new Date().getFullYear()+parseInt(n);
 
-    if(p<=0){
-        alert("Enter a positive value");
+     if(p==""){
+        alert("Enter some principal amount");
         principal.focus();
         return false;
     }
-    else
 
+    if(p==0){
+        alert("Principal amount cannot be zero you dumb !");
+        principal.focus();
+        return false;
+    }
+
+    if(p<0){
+        alert("Principal value cannot be in negative value you dumb !");
+        principal.focus();
+        return false;
+    }
+
+    else
     document.getElementById("result").innerHTML = "If you deposit <marK>"+p+"</mark>,\<br\> at an interest rate of <mark>"+r+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\> in the year <mark>"+year+"</mark>\<br\>";
+
+    return true;
+
 }
 
 function updateRate(){
